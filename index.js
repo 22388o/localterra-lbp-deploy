95969eb2fc0aa8e04a75bedc505c115a600bb9f4
Samuel Barbosa <sambarbosaa@gmail.com>
2021-10-09 02:59:34 -0500

    new contracts

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 import {LCDClient, MnemonicKey, MsgExecuteContract} from "@terra-money/terra.js";
 
 const factoryCfg = {
-  trade_code_id: 12412,
-  token_addr: "terra1at47matt5gjhhcql43m4whs5fmt99pg0py8ze3",
+  trade_code_id: 12451,
+  token_addr: "terra1ys07xmvv0sahqxy6fch38naxjwt09kphw02nlq",
   local_ust_pool_addr: "terra17h9mgy45yht6eg9mvyna52e05nfh8slg6s8tse",
-  gov_addr: "terra1kwkkc8gnty2uanahqkyzf0escqnn3dk7m3rg0w",
-  offers_addr: "terra109c5j4ndlkhvr2aa0ldgxw8rladt22sl96kram",
-  fee_collector_addr: "terra1tssql9k3cy9c7yqu8zjk3cptrfkf8yd2ztg4gh",
-  trading_incentives_addr: "terra14x2ta6wkche2eywd9sh7vefwqdnd8qtj3r84ls"
+  gov_addr: "terra16428xkshv8rh0gxlrt9fhc56yer96cm203a5cr",
+  offers_addr: "terra1gevq5lfzqfd6ntehn29gasacpynklyyge2nqhp",
+  fee_collector_addr: "terra130y7xa3sakpkxy0awam58mglv0r4uln8j36tph",
+  trading_incentives_addr: "terra1e79fm7zjf6enc5cswk90p9q2wrqa40k5j62j6g"
 }
 
 const mk = new MnemonicKey({
