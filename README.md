3555160dd61644cee9174cfa671e5a66b0ca5cf7
Samuel Barbosa <sambarbosaa@gmail.com>
2021-10-21 13:07:48 -0500

    Better logging.

--- README.md
+++ README.md
@@ -3,16 +3,18 @@
 Integration tests that calls Contracts using LCD.
 
 # Instructions
-Run `yarn install` and then `node index.js`.
+Run `yarn install` and then `node index.js` with the desired env vars as described below.
+
+On the 1st run, you must run it with `DEPLOY=all` to upload all the contracts to the chain and generate the code ids for the contracts.
 
 # ENV Variables
-**NETWORK**: (bombay) - default: localterra.
-
 **DEPLOY**: name of contract (name of the wasm file without the extension) or `all` to upload all the contracts.
 
 **CONTRACTS**: the folder with the optmized wasm files.
 
+**FACTORY**: factory address, if Factory is already instantiated you can pass its address on this parameter to skip the deployment process.
+
 Example:
 
-`DEPLOY=factory NETWORK=bombay CONTRACTS=../contracts node index.js`
+`DEPLOY=all CONTRACTS=../contracts/artifacts node index.js`
 
--- index.js
+++ index.js
@@ -6,22 +6,23 @@ import {
   MnemonicKey,
   MsgExecuteContract,
   MsgInstantiateContract,
-  MsgStoreCode,
+  MsgStoreCode
 } from "@terra-money/terra.js";
 import * as fs from "fs";
 import findFilesInDir from "./findFilesInDir.js";
-
+const network = "bombay-12";
+const lcdURL = "https://bombay-lcd.terra.dev";
 const maker_key = new MnemonicKey({
   mnemonic:
-    "uncle simple tide bundle apart absurd tenant fluid slam actor caught month hip tornado cattle regular nerve brand tower boy alert crash good neck",
+    "uncle simple tide bundle apart absurd tenant fluid slam actor caught month hip tornado cattle regular nerve brand tower boy alert crash good neck"
 });
 const taker_key = new MnemonicKey({
   mnemonic:
-    "paddle prefer true embody scissors romance train replace flush rather until clap intact hello used cricket limb cake nut permit toss stove cute easily",
+    "paddle prefer true embody scissors romance train replace flush rather until clap intact hello used cricket limb cake nut permit toss stove cute easily"
 });
 const terra = new LCDClient({
-  URL: "https://bombay-lcd.terra.dev",
-  chainID: "bombay-12",
+  URL: lcdURL,
+  chainID: network
 });
 const maker = maker_key.accAddress;
 const maker_wallet = terra.wallet(maker_key);
@@ -34,9 +35,9 @@ const offer_type = "buy";
 function executeMsg(msg, wallet = maker_wallet) {
   return wallet
     .createAndSignTx({
-      msgs: [msg],
+      msgs: [msg]
     })
-    .then((tx) => {
+    .then(tx => {
       return terra.tx.broadcast(tx);
     });
 }
@@ -51,7 +52,7 @@ function instantiateFactory(codeIds) {
     offer_code_id: codeIds.offer,
     trade_code_id: codeIds.trade,
     fee_collector_threshold: "1000000",
-    local_ust_pool_addr: maker, //TODO: use actual address
+    local_ust_pool_addr: maker //TODO: use actual address
   };
   const instantiateFactoryMsg = new MsgInstantiateContract(
     maker,
@@ -70,12 +71,12 @@ async function test(codeIds) {
   let setup = new Promise((resolve, reject) => {
     if (factoryAddr) {
       console.log("*Querying Factory Config*");
-      terra.wasm.contractQuery(factoryAddr, { config: {} }).then((r) => {
+      terra.wasm.contractQuery(factoryAddr, { config: {} }).then(r => {
         resolve(r);
       });
     } else {
       console.log("*Instantiating Factory*");
-      instantiateFactory(codeIds).then((r) => {
+      instantiateFactory(codeIds).then(r => {
         const factoryAddr = getAttribute(
           r,
           "instantiate_contract",
@@ -83,14 +84,14 @@ async function test(codeIds) {
         );
         console.log("**Factory Addr:", factoryAddr);
         console.log("*Querying Factory Config*");
-        terra.wasm.contractQuery(factoryAddr, { config: {} }).then((r) => {
+        terra.wasm.contractQuery(factoryAddr, { config: {} }).then(r => {
           resolve(r);
         });
       });
     }
   });
   setup
-    .then((r) => {
+    .then(r => {
       factoryCfg = r;
       console.log("Factory Config result", r);
       const newOffer = {
@@ -99,9 +100,9 @@ async function test(codeIds) {
             offer_type,
             fiat_currency: "BRL",
             min_amount,
-            max_amount,
-          },
-        },
+            max_amount
+          }
+        }
       };
       let createOfferMsg = new MsgExecuteContract(
         maker,
@@ -111,7 +112,7 @@ async function test(codeIds) {
       console.log("*Creating Offer*");
       return executeMsg(createOfferMsg);
     })
-    .then((r) => {
+    .then(r => {
       let offerId = getAttribute(r, "from_contract", "id");
       let createTradeMsg = new MsgExecuteContract(
         maker,
@@ -120,22 +121,23 @@ async function test(codeIds) {
           new_trade: {
             offer_id: parseInt(offerId),
             ust_amount: min_amount + "",
-            counterparty: taker,
-          },
+            counterparty: taker
+          }
         }
       );
       console.log("*Creating Trade*");
       return executeMsg(createTradeMsg);
     })
-    .then((result) => {
+    .then(result => {
       tradeAddr = result.logs[0].events
-        .find((e) => e.type === "instantiate_contract")
-        .attributes.find((a) => a.key === "contract_address").value;
+        .find(e => e.type === "instantiate_contract")
+        .attributes.find(a => a.key === "contract_address").value;
       console.log("**Trade created with address:", tradeAddr);
+      console.log(`https://finder.terra.money/${network}/address/${tradeAddr}`);
       //Send UST and fund trade
       const coin = Coin.fromData({
         denom: "uusd",
-        amount: min_amount + "",
+        amount: min_amount + ""
       });
       const coins = new Coins([coin]);
       let fundEscrowMsg = new MsgExecuteContract(
@@ -147,19 +149,20 @@ async function test(codeIds) {
       console.log("*Funding Escrow*");
       return executeMsg(fundEscrowMsg, taker_wallet);
     })
-    .then((r) => {
+    .then(r => {
       if (r.txhash) {
         console.log("**Escrow Funded**");
       } else {
         console.log("%Error%");
       }
       const releaseMsg = new MsgExecuteContract(taker, tradeAddr, {
-        release: {},
+        release: {}
       });
-      executeMsg(releaseMsg, taker_wallet);
+      console.log("*Releasing Trade*");
+      return executeMsg(releaseMsg, taker_wallet);
     })
-    .then((r) => {
-      console.log("Result:", r);
+    .then(r => {
+      console.log("**Trade Released**");
     });
 }
 
@@ -167,14 +170,14 @@ function createStoreMsg(contract) {
   console.log(`*Storing ${contract}*`);
   const wasm = fs.readFileSync(contract, {
     highWaterMark: 16,
-    encoding: "base64",
+    encoding: "base64"
   });
   return new MsgStoreCode(maker, wasm);
 }
 
 function getContractNameFromPath(path) {
-  let regex = RegExp(/artifacts\/(.*?)\.wasm/, 'i')
-  return path.match(regex)[1]
+  let regex = RegExp(/artifacts\/(.*?)\.wasm/, "i");
+  return path.match(regex)[1];
 }
 
 function getCodeIdFromResult(result) {
@@ -183,8 +186,8 @@ function getCodeIdFromResult(result) {
 
 function getAttribute(result, event, attribute) {
   return result.logs[0].events
-    .find((e) => e.type === event)
-    .attributes.find((e) => e.key === attribute).value;
+    .find(e => e.type === event)
+    .attributes.find(e => e.key === attribute).value;
 }
 async function deploy(contract) {
   let codeIds = {};
@@ -227,7 +230,7 @@ async function deploy(contract) {
 function fundEscrow(tradeAddr) {
   const coin = Coin.fromData({
     denom: "uusd",
-    amount: min_amount + "",
+    amount: min_amount + ""
   });
   const coins = new Coins([coin]);
   let fundEscrowMsg = new MsgExecuteContract(
@@ -237,7 +240,7 @@ function fundEscrow(tradeAddr) {
     coins
   );
   console.log("*Funding Escrow*");
-  executeMsg(fundEscrowMsg, taker_wallet).then((r) => {
+  executeMsg(fundEscrowMsg, taker_wallet).then(r => {
     console.log("Result", r);
     if (r.txhash) {
       release(tradeAddr, taker_wallet);
@@ -248,11 +251,11 @@ function fundEscrow(tradeAddr) {
 function release(tradeAddr, wallet) {
   console.log("Sending release msg");
   const releaseMsg = new MsgExecuteContract(taker, tradeAddr, {
-    release: {},
+    release: {}
   });
   console.log("Release Msg:", releaseMsg);
-  executeMsg(releaseMsg, wallet).then((r) => {
-    r.toJSON().then((r) => console.log(r));
+  executeMsg(releaseMsg, wallet).then(r => {
+    r.toJSON().then(r => console.log(r));
   });
 }
 
